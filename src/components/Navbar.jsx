9f5421aa957157a0d3733f0f090d6a3a8b7c0e5b
Aditya Patil <adityapatil687@gmail.com>
2024-10-26 19:02:11 +0530

    spliced into small components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
-import './styles/Navbar.css';
+import "./styles/Navbar.css";
 
 function Navbar() {
   // State to manage the icon and theme
   const [isDarkMode, setIsDarkMode] = useState(true);
-  const [showOffcanvas, setShowOffcanvas] = useState(false); // State to control offcanvas visibility
+
+  // State to control offcanvas visibility
+  const [showOffcanvas, setShowOffcanvas] = useState(false);
 
   // Effect to update the body attribute for theme
   useEffect(() => {
@@ -27,53 +29,102 @@ function Navbar() {
         <div className="container-fluid">
           <div className="d-inline-flex mb-1 px-2">
             {/* Toggle Button: Only show on small screens */}
-            <div className="sideMenuBtn d-lg-none">
-              {/* Hide on large screens */}
-              <button className="btn mt-1 me-4" onClick={handleOffcanvasToggle}>
-                <i className="bi bi-list fs-4"></i>
-              </button>
-            </div>
-            <a className="navbar-brand" href="#">
-              <img
-                src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500"
-                alt="Bootstrap"
-                width={40}
-              />
-            </a>
-            <a className="navbar-brand text-light mt-1">
-              CodeBase
-              <span className="rounded px-1 custom-title-border">DEV</span>
-            </a>
+            <OffcanvasButton handleOffcanvasToggle={handleOffcanvasToggle} />
+            <NavbarBrand />
           </div>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0 py-1">
-              <li className="nav-item mx-2">
-                <a className="nav-link active text-light nav-link-custom rounded" aria-current="page" href="#">
-                  Topics
-                </a>
-              </li>
-              <li className="nav-item mx-2">
-                <a className="nav-link active text-light nav-link-custom rounded" aria-current="page" href="#">
-                  Docs
-                </a>
-              </li>
-              <li className="nav-item mx-2">
-                <a className="nav-link active text-light nav-link-custom rounded" aria-current="page" href="#">
-                  About
-                </a>
-              </li>
-            </ul>
-          </div>
-          <button
-            onClick={handleToggle}
-            className="btn rounded text-light me-2 rounded-circle"
-          >
-            <i className={`bi ${isDarkMode ? "bi-moon" : "bi-sun"} h5 mt-2`}></i>
-          </button>
+          <NavbarLinks />
+          <ThemeButton handleToggle={handleToggle} isDarkMode={isDarkMode} />
         </div>
       </nav>
 
       {/* Offcanvas Sidebar */}
+      <OffcanvasSidebar
+        showOffcanvas={showOffcanvas}
+        handleOffcanvasToggle={handleOffcanvasToggle}
+      />
+    </>
+  );
+}
+
+function NavbarBrand() {
+  return (
+    <>
+      <a className="navbar-brand" href="#">
+        <img
+          src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500"
+          alt="Bootstrap"
+          width={40}
+        />
+      </a>
+      <a className="navbar-brand text-light mt-1">
+        CodeBase
+        <span className="rounded px-1 custom-title-border">DEV</span>
+      </a>
+    </>
+  );
+}
+function NavbarLinks() {
+  return (
+    <>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0 py-1">
+          <li className="nav-item mx-2">
+            <a
+              className="nav-link active text-light nav-link-custom rounded"
+              aria-current="page"
+              href="#"
+            >
+              Topics
+            </a>
+          </li>
+          <li className="nav-item mx-2">
+            <a
+              className="nav-link active text-light nav-link-custom rounded"
+              aria-current="page"
+              href="#"
+            >
+              Docs
+            </a>
+          </li>
+          <li className="nav-item mx-2">
+            <a
+              className="nav-link active text-light nav-link-custom rounded"
+              aria-current="page"
+              href="#"
+            >
+              About
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
+function OffcanvasButton({ handleOffcanvasToggle }) {
+  return (
+    <>
+      <div className="sideMenuBtn d-lg-none">
+        {/* Hide on large screens */}
+        <button className="btn mt-1 me-4" onClick={handleOffcanvasToggle}>
+          <i className="bi bi-list fs-4"></i>
+        </button>
+      </div>
+    </>
+  );
+}
+function ThemeButton({ handleToggle, isDarkMode }) {
+  return (
+    <button
+      onClick={handleToggle}
+      className="btn rounded text-light me-2 rounded-circle"
+    >
+      <i className={`bi ${isDarkMode ? "bi-moon" : "bi-sun"} h5 mt-2`}></i>
+    </button>
+  );
+}
+function OffcanvasSidebar({ showOffcanvas, handleOffcanvasToggle }) {
+  return (
+    <>
       <div
         className={`offcanvas offcanvas-start ${showOffcanvas ? "show" : ""}`}
         tabIndex="-1"
@@ -90,7 +141,8 @@ function Navbar() {
                 width={40}
               />
             </a>
-            <a className="navbar-brand text-light mt-2 ms-2 h4">CodeBase
+            <a className="navbar-brand text-light mt-2 ms-2 h4">
+              CodeBase
               <span className="rounded px-1 custom-title-border">DEV</span>
             </a>
           </div>
@@ -103,17 +155,26 @@ function Navbar() {
         <div className="offcanvas-body">
           <ul className="navbar-nav flex-column">
             <li className="nav-item">
-              <a className="nav-link nav-link-custom text-light rounded" href="#">
+              <a
+                className="nav-link nav-link-custom text-light rounded"
+                href="#"
+              >
                 <span className="ms-3">Topics</span>
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link nav-link-custom text-light rounded" href="#">
+              <a
+                className="nav-link nav-link-custom text-light rounded"
+                href="#"
+              >
                 <span className="ms-3">Docs</span>
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link nav-link-custom text-light rounded" href="#">
+              <a
+                className="nav-link nav-link-custom text-light rounded"
+                href="#"
+              >
                 <span className="ms-3">About</span>
               </a>
             </li>
