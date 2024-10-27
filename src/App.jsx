4611d43b8e77bd8171618d13be826542de56ad9e
Aditya Patil <adityapatil687@gmail.com>
2024-10-27 13:04:25 +0530

    Seperated App Routes in seperate file

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,19 @@
 // App.js
-import React from 'react';
-import { createPortal } from 'react-dom';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { createPortal } from "react-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 // Components
-import Navbar from './components/Navbar/Navbar';
+import Navbar from "./components/Navbar/Navbar";
 
-// Pages
-import Home from './pages/Home';
-import Topics from './pages/Topics';
-import About from './pages/About';
-import Docs from './pages/Docs';
+// Routes
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
     <Router>
-      {/* Render Navbar inside a specific <nav> element using createPortal */}
-      {createPortal(<Navbar />, document.getElementById('navbar'))}
-
-      {/* Define Routes for each page */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/topics" element={<Topics />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/docs" element={<Docs />} />
-      </Routes>
+      {createPortal(<Navbar />, document.getElementById("navbar"))}
+      <AppRoutes />
     </Router>
   );
 }
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+// Pages
+import Home from '../pages/Home';
+import Topics from '../pages/Topics';
+import About from '../pages/About';
+import Docs from '../pages/Docs';
+
+const AppRoutes = () => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/topics/*" element={<Topics />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/docs" element={<Docs />} />
+  </Routes>
+);
+
+export default AppRoutes;
